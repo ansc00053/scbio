562d0ee8810d0c2b37fce7c501e1af8394ef72e2
ansc00053 <ansc00053@uni-saarland.de>
2024-11-21 14:40:26 +0100

    Rough draft

--- week1.R
+++ week1.R
@@ -14,7 +14,10 @@ install.packages("Cairo")
 install.packages("tidyverse")
 install.packages("ggplot2")
 install.packages("gridExtra")
+install.packages("viridis")
 
+
+library(viridis)
 library(gridExtra)
 library(ggplot2)
 library(Cairo)
@@ -51,12 +54,94 @@ ArrowFiles <- createArrowFiles(
   verbose = TRUE
 )
 
-ArrowFiles <- c("hft_ctx_w21_dc1r3_r1_atac_fragments.arrow", "hft_ctx_w21_dc2r2_r1_atac_fragments.arrow", "hft_ctx_w21_dc2r2_r2_atac_fragments.arrow")
 ArrowFiles # check to see if data loaded properly 
 
-# Create ArchR project
+# 1.3) Identify Doublets
+
+# Add doublet scores to the project
+doubScores <- addDoubletScores(
+  input = ArrowFiles,
+  k = 10,            # Number of nearest neighbors to consider for pseudo-doublets
+  knnMethod = "UMAP", # Embedding method for nearest neighbor search
+  LSIMethod = 1      # Method for inferring doublets (set to 1 as default)
+)
+
+# 1.4) Create ArchR project
 proj <- ArchRProject(ArrowFiles = ArrowFiles, outputDirectory = "ArchR_Project")
 
+# 1.4) Inspect the project and cell metadata
+proj
+
+colnames(proj@cellColData) # metadata names, frags = nFrags and TSS = ReadsInTSS
+
+# Getting peak set from data
+
+proj_peaks <- getPeakSet(proj) #GRanges object
+
+head(proj_peaks) # glimpse at peaks
+
+proj <- addPeakSet(
+  ArchRProj = proj,
+  peakSet = proj_peaks,
+  genomeAnnotation = getGenomeAnnotation(proj),
+  force = TRUE # allows us to overwrite old peaks 
+)
+
+# Filter out doublets based on the threshold
+proj_filtered <- proj[proj$DoubletScore < 0.25, ] # threshold = 0.25, the default, or whatever is fit
+
+# Check the filtered project
+proj_filtered
+
+colnames(proj_filtered@cellColData) # metadata names, frags = nFrags and TSS = ReadsInTSS
+
+# save the filtered project to a new file
+saveArchRProject(proj_filtered, "c:/Users/ArchR/proj_doubletfiltered")
+
+#### This is data after QC filtering and doublet removal ## 
+# 1.4)  Number of cells in the project
+num_cells <- sum(table(proj_filtered@cellColData$Sample))
+cat("Number of cells in the project: \n")
+print(num_cells)
+
+# 1.4) Median TSS value and median number of fragments
+median_tss <- median(proj_filtered@cellColData$ReadsInTSS)
+median_frags <- median(proj_filtered@cellColData$nFrags)
+cat("Median TSS value:", median_tss, "\n")
+cat("Median number of fragments:", median_frags, "\n")
+
+# 1.4) Dimensions of the peak set (number of peaks)
+num_peaks <- nrow(proj_filtered@peakSet)
+cat("Number of peaks in the dataset:", num_peaks, "\n")
+
+# 1.4) Number of cells per sample
+cells_per_sample <- table(proj_filtered@cellColData$Sample)
+cat("Number of cells per sample:\n")
+print(cells_per_sample)
+
+# 1.5) Plot the fragment length distribution of all samples in one plot
+plotFragmentLengths(proj_filtered, 
+                    colorBy = "Sample", 
+                    pal = viridis(100), 
+                    plotTitle = "Fragment Length Distribution"
+                    )
+
+# 1.5)´ Plot the distribution of TSS enrichment scores for each sample
+plotTSSEnrichment(
+  ArchRProj = proj_filtered,
+  groupBy = "Sample",
+  chromSizes = getChromSizes(proj),
+  TSS = getTSS(proj),
+  pal = viridis(100),
+  returnDF = FALSE,
+  threads = getArchRThreads(),
+  logFile = createLogFile("plotTSSEnrichment")
+)
+
+# log10(nFrags) v TSS enrichment score plots generated upon arrow file creation
+
+
+####################################
 ### IGNORE THIS IF U HAVE A MAC #### 
 # Retrieve all cell metadata
 allCellData <- getCellColData(proj, select = c("Sample", "nFrags", "TSSEnrichment"))
@@ -106,26 +191,5 @@ print(combinedFragSizePlot)
 ggsave("Log10nFrags_vs_TSSEnrichment_AllSamples.pdf", plot = combinedLog10nFragsPlot, height = 12, width = 6)
 ggsave("FragmentSize_Distribution_AllSamples.pdf", plot = combinedFragSizePlot, height = 12, width = 6)
 ### IGNORE THIS IF U HAVE A MAC #### 
+####################################
 
-# 1.3) Identify Doublets
-
-# Add doublet scores to the project
-doubScores <- addDoubletScores(
-  input = ArrowFiles,
-  k = 10,            # Number of nearest neighbors to consider for pseudo-doublets
-  knnMethod = "UMAP", # Embedding method for nearest neighbor search
-  LSIMethod = 1      # Method for inferring doublets (set to 1 as default)
-)
-
-# Visualize the doublet scores
-plotEmbedding(proj, colorBy = "cellColData", name = "DoubletScore")
-
-# Set threshold for doublets
-proj <- proj[!proj$DoubletScore > 0.25,]
-
-# Check the number of cells after removal
-ncol(proj)
-
-# Number of doublets detected before and after removal
-sum(proj$DoubletScore > 0.25)  # Number of detected doublets
-ncol(proj)  # Number of remaining cells
